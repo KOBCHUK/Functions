9d2ddeefe7fea99a857a8313d505144bdbfafd8e
Eugen Kobchuk <irravadi@gmail.com>
2023-04-15 17:36:10 +0300

    ex_8_Yevhen

--- Functions.py
+++ Functions.py
@@ -1,1 +1,6 @@
-
+def sort_by_age(list_of_tuples):
+    sorted_list = sorted(list_of_tuples, key=lambda x: x[1])
+    return sorted_list
+list_of_tuples = [('Yevhen', 44), ('Maria', 7), ('Ivan', 7)]
+sorted_list = sort_by_age(list_of_tuples)
+print(sorted_list)
