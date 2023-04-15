a901d3ea850418858d61891a97339d9ba9c664ce
Eugen Kobchuk <irravadi@gmail.com>
2023-04-15 17:27:34 +0300

    ex_2_Yevhen

--- Functions.py
+++ Functions.py
@@ -1,1 +1,10 @@
-
+def count_vowels_and_consonants(string):
+    vowels = 0
+    consonants = 0
+    for i in string:
+        if i in "aeiouAEIOU":
+            vowels += 1
+        elif i in "bcdfghjklmnpqrstvwxyzBCDFGHJKLMNPQRSTVWXYZ":
+            consonants += 1
+    print(f"There are {vowels} vowels and {consonants} consonants in the string.")
+    return vowels, 
