83d0fc78c9b77bd089a68578709207ade7b0e2a5
Eugen Kobchuk <irravadi@gmail.com>
2023-04-15 17:33:48 +0300

    ex_6_Yevhen

--- Functions.py
+++ Functions.py
@@ -1,1 +1,10 @@
+def scalar_product(list1, list2):
+    if len(list1) != len(list2):
+        raise ValueError("The two lists must have the same length.")
 
+    return sum([list1[i] * list2[i] for i in range(len(list1))])
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+result = scalar_product(list1, list2)
+print(result)
