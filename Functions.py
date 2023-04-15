cc2a09fe3dd59b456f46ade784df317d7a954f32
Eugen Kobchuk <irravadi@gmail.com>
2023-04-15 17:29:39 +0300

    ex_4_Yevhen

--- Functions.py
+++ Functions.py
@@ -1,1 +1,12 @@
-
+def fibonacci(n):
+    if n <= 0:
+        return None
+    elif n == 1:
+        return 0
+    elif n == 2:
+        return 1
+    else:
+        return fibonacci(n - 1) + fibonacci(n - 2)
+n = 10
+fib_n = fibonacci(n)
+print(f"The {n}th element of the Fibonacci sequence is: {fib_n}")
