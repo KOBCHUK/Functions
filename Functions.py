e3b8d4e98495625ca2447fec8ed1a92ad220d756
Eugen Kobchuk <irravadi@gmail.com>
2023-04-15 17:38:23 +0300

    ex_10_Yevhen

--- Functions.py
+++ Functions.py
@@ -1,1 +1,18 @@
-
+def most_frequent(numbers):
+    freq = {}
+    for num in numbers:
+        if num in freq:
+            freq[num] += 1
+        else:
+            freq[num] = 1
+    max_freq = 0
+    most_common_num = None
+    for num, f in freq.items():
+        if f > max_freq:
+            max_freq = f
+            most_common_num = num
+        elif f == max_freq:
+            most_common_num = min(most_common_num, num)
+    return most_common_num
+numbers = [1, 2, 3, 2, 2, 4, 5, 4, 4, 4]
+print(most_frequent(numbers))
