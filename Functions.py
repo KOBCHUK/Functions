41671cbfd03961af22dfa4ac9bb69d68592e1ac9
Bohdan <bohdanstep23@gmail.com>
2023-04-19 14:19:53 +0300

    Done

--- Functions.py
+++ Functions.py
@@ -1,1 +1,8 @@
+def first_last_three_chars(s):
+    if len(s) < 6:
+        return "nedostatnyo symbols"
+    else:
+        return s[:3] + s[-3:]
 
+s = "Nvidia Geforce"
+print(first_last_three_chars(s)) 
